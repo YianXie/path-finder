0cae4358c7bc4507e70bac2229bcb49f822f3021
YianXie <yian.xie@outlook.com>
2025-10-11 19:14:52 +0800

    update render-build.sh

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit on error
+set -o errexit
+
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
@@ -8,8 +11,3 @@ python manage.py collectstatic --no-input
 
 echo "Migrating database..."
 python manage.py migrate
-
-echo "Running server..."
-gunicorn pathfinder_api.wsgi:application --bind 0.0.0.0:8000 --preload --timeout 120
-
-echo "Server running on port 8000"
