db33f5dec3806dcf36460f59ac72e5f0c116bd30
YianXie <yian.xie@outlook.com>
2025-10-17 11:19:48 +0800

    fix: one-line fix for undefuned view

--- backend/suggestions/urls.py
+++ backend/suggestions/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path("health/", views.HealthCheck.as_view(), name="health_check"),
-    path("test-jwt/", views.TestJWTAuthentication.as_view(), name="test_jwt"),
     path("suggestions/", views.GetSuggestions.as_view(), name="get_suggestions"),
 ]
