d950abdf174c79bb1aac607b295604715dcde3d3
YianXie <yian.xie@outlook.com>
2025-10-25 18:07:19 +0800

    feat: :speech_balloon: added a line that informs user when he/she has no saved item
    added a line of code in Saved.jsx that informs the user when there is no saved items

--- frontend/src/pages/auth/Saved.jsx
+++ frontend/src/pages/auth/Saved.jsx
@@ -1,4 +1,5 @@
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 import api from "../../api";
 import { LoadingBackdrop, PageHeader } from "../../components/common";
@@ -18,17 +19,32 @@ function Saved() {
         <>
             <LoadingBackdrop open={isLoading} />
             <PageHeader title="Saved" subtitle="Saved items" className="my-6" />
-            <Grid
-                container
-                spacing={6}
-                justifyContent="center"
-                alignItems="center"
-                marginTop={6}
-            >
-                {savedItems?.map((item) => (
-                    <Item key={item.external_id} {...item} is_saved={true} />
-                ))}
-            </Grid>
+            {savedItems?.length <= 0 ? (
+                <Typography
+                    variant="h6"
+                    textAlign="center"
+                    sx={{ marginTop: 4 }}
+                >
+                    You have no saved items. Start saving items to your account
+                    to view them here.
+                </Typography>
+            ) : (
+                <Grid
+                    container
+                    spacing={6}
+                    justifyContent="center"
+                    alignItems="center"
+                    marginTop={6}
+                >
+                    {savedItems?.map((item) => (
+                        <Item
+                            key={item.external_id}
+                            {...item}
+                            is_saved={true}
+                        />
+                    ))}
+                </Grid>
+            )}
         </>
     );
 }
