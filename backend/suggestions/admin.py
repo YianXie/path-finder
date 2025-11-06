552543a40ddcd25d60058351332af8433149006a
Aryan Mahajan <mahajan802464@sas.edu.sg>
2025-11-06 11:41:01 +0800

    fix: typo in sugggestions/admin

--- backend/suggestions/admin.py
+++ backend/suggestions/admin.py
@@ -3,5 +3,4 @@ from django.contrib import admin
 from .models import SuggestionModel, SuggestionsCacheModel
 
 admin.site.register(SuggestionModel)
-admin.site.register(SuggestionCacheModel)
 admin.site.register(SuggestionsCacheModel)
