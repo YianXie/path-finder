231c8507f03cccb2277ecd73ddf95dc6e9826583
YianXie <yian.xie@outlook.com>
2025-11-15 14:14:01 +0800

    build: updated render-build.sh and .gitignore

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
-
-# Exit on error
 set -e
 
 echo "Installing dependencies..."
 
 mkdir -p .uvbin
 
-# Install uv
+# Install uv locally
 curl -LsSf https://astral.sh/uv/install.sh | UV_INSTALL_DIR=".uvbin" sh
 
-# Set path
-export PATH="$PATH:$(pwd)/.uvbin"
+# Put uv on PATH
+export PATH="$PWD/.uvbin:$PATH"
 
-# Make venv and install packages
-echo "Making venv..."
-if [ ! -d ".venv" ]; then uv venv; fi # if venv doesn't exist, create it
+echo "Syncing dependencies and creating venv..."
+uv sync  # this will create .venv if needed and install everything
 
 echo "Activating venv..."
 source .venv/bin/activate
 
-echo "Syncing dependencies..."
-uv sync
-
-# Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
-# Migrate database
 echo "Migrating database..."
-python manage.py makemigrations && python manage.py migrate
+python manage.py makemigrations --no-input || true
+python manage.py migrate --no-input
 
-# Sync suggestions
 echo "Syncing suggestions..."
 python manage.py sync_sheet
