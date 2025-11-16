35dffefd35208123dfebccd15834bba16d6a7400
YianXie <yian.xie@outlook.com>
2025-11-16 17:26:14 +0800

    fix: formatted code

--- backend/pathfinder_api/urls.py
+++ backend/pathfinder_api/urls.py
@@ -1,9 +1,9 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
